a7d6a6708315c3681d8971d097f536482221fba9
devanshkanda <devanshkanda02@gmail.com>
2024-05-13 01:15:44 +0530

    added updates to likes

--- src/routes/likes_routes.py
+++ src/routes/likes_routes.py
@@ -2,13 +2,13 @@ from fastapi.routing import APIRouter
 from models.user_model import Likes
 from main import db
 
-likes_router = APIRouter(
+router = APIRouter(
     prefix="/likes"
 )
 
 likes_collection = db["likes"]
 
-@likes_router.get("/{blog_id}/likes")
+@router.get("/{blog_id}/likes")
 async def get_likes_on_post(blog_id: str):
     likes = likes_collection.find({"post_id": blog_id})
 
@@ -19,7 +19,7 @@ async def get_likes_on_post(blog_id: str):
         "likes": len(likes)
     }
 
-@likes_router.post("/{blog_id}")
+@router.post("/{blog_id}")
 async def like_a_post(like: Likes, blog_id: str):
     like_id = str(likes_collection.insert_one(dict(like)))
 
